27cb88948948e928b9c2e1fd1c87ded727c822ac
Elysia Webb <elysia.n.webb@gmail.com>
2021-08-03 16:24:15 -0400

    trying to build out admin view on front

---
+++ vue/src/views/Admin.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+      <active-requests/>
+  </div>
+</template>
+
+<script>
+import ActiveRequests from '../components/ActiveRequests.vue'
+export default {
+  components: { ActiveRequests },
+
+}
+</script>
+
+<style>
+
+</style>
