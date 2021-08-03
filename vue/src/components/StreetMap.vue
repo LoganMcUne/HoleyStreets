80402f630203fad7ee8de02ce7886e9ad47bd858
John Sliney <jjs061090@gmail.com>
2021-08-03 15:13:29 -0400

    working more map

--- vue/src/components/StreetMap.vue
+++ vue/src/components/StreetMap.vue
@@ -1,29 +1,32 @@
 <template>
-  <div style="height: 1000px; width: 1000px">
+
+  <div id="ourmap" style="height: 80vh; width: 80vw">
     <l-map
+      v-if="showMap"
       :zoom="zoom"
       :center="center"
       :options="mapOptions"
-      style="height: 95%"
+      style="height: 90%"
       @update:center="centerUpdate"
+      @update:zoom="zoomUpdate"
     >
-      <l-tile-layer :url="url" :attribution="attribution" />
-      <l-marker
-        :key="pothole.id"
-        v-for="pothole in $store.state.potholes"
-        :lat-lng="[pothole.latitude, pothole.longitude]"
+      <l-tile-layer
+        :url="url"
+        :attribution="attribution"
       />
+      <l-marker v-bind:key ="pothole.id" v-for="pothole in $store.state.potholes" :lat-lng="makeLatLng(pothole.latitude, pothole.longitude)">
+      </l-marker>
     </l-map>
-    {{ currentCenter }}
+    {{ currentCenter }} {{ currentZoom }}
   </div>
 </template>
 
 <script>
-import L from "leaflet"
-import { LMap, LTileLayer, LMarker } from "vue2-leaflet";
+import { latLng } from "leaflet";
+import { LMap, LTileLayer, LMarker} from "vue2-leaflet";
 
 export default {
-  name: "HoleyMap",
+  name: "Example",
   components: {
     LMap,
     LTileLayer,
@@ -31,19 +34,44 @@ export default {
   },
   data() {
     return {
-      map: null,
-      zoom: 15,
-      center: [39.151983, -84.467922],
-      url: "https://api.maptiler.com/maps/basic/{z}/{x}/{y}.png?key=92OMGZCxPkxka0uiFn9y",
+      zoom: 13,
+      center: latLng(39.15949,	-84.455277),
+      url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
       attribution:
         '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
-      currentCenter: [39.151983, -84.467922],
+      withPopup: latLng(47.41322, -1.219482),
+      withTooltip: latLng(47.41422, -1.250482),
+      currentZoom: 11.5,
+      currentCenter: latLng(47.41322, -1.219482),
+      showParagraph: false,
+      mapOptions: {
+        zoomSnap: 0.5
+      },
+      showMap: true
     };
   },
   methods: {
+    zoomUpdate(zoom) {
+      this.currentZoom = zoom;
+    },
     centerUpdate(center) {
       this.currentCenter = center;
     },
-  },
+    showLongText() {
+      this.showParagraph = !this.showParagraph;
+    },
+    innerClick() {
+      alert("Click!");
+    },
+    makeLatLng(lat, lng){
+      return latLng(lat, lng);
+    }
+  }
 };
 </script>
+
+<style scoped>
+#ourmap{
+  margin: 20px auto 0px ;
+}
+</style>
