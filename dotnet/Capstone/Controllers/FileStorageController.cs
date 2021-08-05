5f3665f934f92a0352ec9f1778821a911ec29ce6
Aaron Smith <aaron.j.smith2019@gmail.com>
2021-08-05 12:02:07 -0400

    Adds file storage in back-end API

---
+++ dotnet/Capstone/Controllers/FileStorageController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Capstone.DAO;
+using Capstone.Models;
+using Capstone.Security;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Capstone.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class FileStorageController : ControllerBase
+    {
+        private readonly IFileStorageDao fileDao;
+
+        public FileStorageController(IFileStorageDao _fileDao)
+        {
+            fileDao = _fileDao;
+        }
+    }
+}
