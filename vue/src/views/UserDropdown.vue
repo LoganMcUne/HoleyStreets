2900e1afa1a4bb8987c4936e178b6aaee35b7fcb
John Sliney <jjs061090@gmail.com>
2021-08-06 11:20:12 -0400

    changed dropdown so all is link

--- vue/src/views/UserDropdown.vue
+++ vue/src/views/UserDropdown.vue
@@ -1,44 +1,60 @@
 <template>
   <div>
-    <b-dropdown id="dropdown-right" right text="User Options" variant="light" class="m-md-2">
-      <b-dropdown-item v-if="$store.state.token != ''">
-        <router-link
-          v-bind:to="{ name: 'logout' }"
-          class="dropdown-btn">
-          Logout
-        </router-link>
-      </b-dropdown-item>
-      <b-dropdown-item  v-if="$store.state.token == ''">
-        <router-link
-          v-bind:to="{ name: 'login' }"
-          class="dropdown-btn">
-          Login
-        </router-link>
-      </b-dropdown-item>
-      <b-dropdown-item  v-if="$store.state.token != ''">
-        <router-link
-          v-bind:to="{ name: 'account' }"
-          class="dropdown-btn">
-          View Account
-        </router-link>
-      </b-dropdown-item>
-       <b-dropdown-item v-if="$store.state.user.role == 'admin'">
-        <router-link
-          v-bind:to="{ name: 'admin' }"
-          class="dropdown-btn">
-          View Admin Page
-        </router-link>
-      </b-dropdown-item>
+    <b-dropdown
+      id="dropdown-right"
+      right
+      text="User Options"
+      variant="light"
+      class="m-md-2"
+    >
+      <router-link
+        v-if="$store.state.token != ''"
+        v-bind:to="{ name: 'logout' }"
+        tag="b-dropdown-item"
+        class="dropdown-btn"
+      >
+        Logout
+      </router-link>
+
+      <router-link
+        v-if="$store.state.token == ''"
+        v-bind:to="{ name: 'login' }"
+        tag="b-dropdown-item"
+        class="dropdown-btn"
+      >
+        Login
+      </router-link>
+
+      <router-link
+        v-if="$store.state.token != ''"
+        v-bind:to="{ name: 'account' }"
+        tag="b-dropdown-item"
+        class="dropdown-btn"
+      >
+        View Account
+      </router-link>
+
+      <router-link
+        v-if="$store.state.user.role == 'admin'"
+        tag="b-dropdown-item"
+        v-bind:to="{ name: 'admin' }"
+        class="dropdown-btn"
+      >
+        View Admin Page
+      </router-link>
+
       <b-dropdown-item v-if="$store.state.user.role == 'user'">
-          <request-employment/>
+        <request-employment />
       </b-dropdown-item>
-      <b-dropdown-item v-if="$store.state.user.role == 'employee'">
-        <router-link
-          v-bind:to="{ name: 'employee' }"
-          class="dropdown-btn">
-          View Employee Page
-        </router-link>
-      </b-dropdown-item>
+
+      <router-link
+        v-if="$store.state.user.role == 'employee'"
+        tag="b-dropdown-item"
+        v-bind:to="{ name: 'employee' }"
+        class="dropdown-btn"
+      >
+        View Employee Page
+      </router-link>
     </b-dropdown>
   </div>
 </template>
@@ -46,19 +62,15 @@
 <script>
 import RequestEmployment from "../components/RequestEmployment.vue";
 
-export default{
-  components: { RequestEmployment }
-}
-
+export default {
+  components: { RequestEmployment },
+};
 </script>
 
 <style>
-
 .dropdown-btn {
   text-decoration: none;
   color: black;
   justify-content: flex-end;
-  margin: 0px 10px;
 }
-
 </style>
