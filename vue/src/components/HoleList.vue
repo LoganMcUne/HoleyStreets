03b659afccd1060b8f6a34c3843866683dd4fc17
Logan McUne <logan.mcune@gmail.com>
2021-08-09 11:21:39 -0400

    Added filtering in Account View

--- vue/src/components/HoleList.vue
+++ vue/src/components/HoleList.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="list-of-potholes">
-    <b-table :items="potholes"
+    <b-table 
+       :items="potholes" 
+       :fields="visibleFields"
        bordered 
        striped
        sticky-header
@@ -13,14 +15,40 @@
 
 export default {
   name: "hole-list",
-  props: ["potholes"]
-};
+  props: ["potholes"],
+  data() {
+    return {
+      fields: [
+        { key: 'id', visible: false },
+        { key: 'latitude', visible: true },
+        { key: 'longitude', visible: true },
+        { key: 'imageLink', visible: false },
+        { key: 'reportedDate', visible: true },
+        { key: 'reportingUserId', visible: false },
+        { key: 'inspectedDate', visible: true },
+        { key: 'repairedDate', visible: true },
+        { key: 'repairStatus', visible: true },
+        { key: 'severity', visible: false },
+        { key: 'isBig', visible: false },
+      ]
+    }
+  },
+  computed: {
+      visibleFields() {
+        return this.fields.filter(field => field.visible);
+      },
+ }
+} 
 </script>
 
 <style>
 
 .list-of-potholes {
   margin: 5px;
+}
+
+th {
+  text-transform: lowercase;
 }
 
 </style>
