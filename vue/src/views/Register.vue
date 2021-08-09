eaa6f023992ec9c3950bcd445833184e76d43549
Aaron Smith <aaron.j.smith2019@gmail.com>
2021-08-09 14:00:51 -0400

    Styles register page

--- vue/src/views/Register.vue
+++ vue/src/views/Register.vue
@@ -1,44 +1,46 @@
 <template>
   <div id="register" class="text-center">
-    <form class="form-register" @submit.prevent="register">
-      <h1 class="h3 mb-3 font-weight-normal">Create Account</h1>
-      <router-link :to="{ name: 'login' }">Have an account?</router-link><br>
-      <div class="alert alert-danger" role="alert" v-if="registrationErrors">
-        {{ registrationErrorMsg }}
-      </div><br>
-      <label for="username" class="sr-only">Username</label>
-      <input
-        type="text"
-        id="username"
-        class="form-control"
-        placeholder="Username"
-        v-model="user.username"
-        required
-        autofocus
-      /><br>
-      <label for="password" class="sr-only">Password</label>
-      <input
-        type="password"
-        id="password"
-        class="form-control"
-        placeholder="Password"
-        v-model="user.password"
-        required
-      /><br>
-      <input
-        type="password"
-        id="confirmPassword"
-        class="form-control"
-        placeholder="Confirm Password"
-        v-model="user.confirmPassword"
-        required
-      />
-     <br>
-      <button class="button" type="submit">
-        Create Account
-      </button>
-   
-    </form>
+    <div class="register-form-background">
+      <form class="form-register" @submit.prevent="register">
+        <h1 class="h3 mb-3 font-weight-normal">Create Account</h1>
+        <router-link :to="{ name: 'login' }">Have an account?</router-link><br>
+        <div class="alert alert-danger" role="alert" v-if="registrationErrors">
+          {{ registrationErrorMsg }}
+        </div><br>
+        <label for="username" class="sr-only">Username</label>
+        <input
+          type="text"
+          id="username"
+          class="form-control"
+          placeholder="Username"
+          v-model="user.username"
+          required
+          autofocus
+        /><br>
+        <label for="password" class="sr-only">Password</label>
+        <input
+          type="password"
+          id="password"
+          class="form-control"
+          placeholder="Password"
+          v-model="user.password"
+          required
+        /><br>
+        <input
+          type="password"
+          id="confirmPassword"
+          class="form-control"
+          placeholder="Confirm Password"
+          v-model="user.confirmPassword"
+          required
+        />
+      <br>
+        <button class="button" type="submit">
+          Create Account
+        </button>
+    
+      </form>
+    </div>
   </div>
 </template>
 
@@ -97,10 +99,13 @@ export default {
   display: flex;
   justify-content: center;
   align-items: center;
-  border: 2px solid black;
-  height: 75vh;
-  margin: 100px 200px;
-  background-color: #f0ead2;
+  margin-top: 8vh;
+  padding: 10vh;
+  border-radius: 10px;
+  background-image: url("../../public/road_pic_3.jpg");
+  background-size: contain;
+  background-repeat: no-repeat;
+  background-position: center;
 }
 
 button {
@@ -113,6 +118,14 @@ button {
   background-color: lightblue;
   box-shadow: 0 5px #666;
   transform: translateY(4px);
+}
+
+.register-form-background {
+  display:flex;
+  border: 2px solid black;
+  border-radius: 10px;
+  padding: 2vh;
+  background-color: #f0ead2;
 }
 
 </style>
