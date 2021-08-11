e6c7d9924dc9ffef9d350d830d42bda41f67f6df
Logan McUne <logan.mcune@gmail.com>
2021-08-11 14:55:35 -0400

    Added functionality to change the status of a claim via Admin Page

--- vue/src/components/AllClaims.vue
+++ vue/src/components/AllClaims.vue
@@ -3,46 +3,65 @@
     <div class="employee-access-table">
       <h1 class="admin-h1">Damage Claims</h1>
       <div class="scroll-section">
-      <table>
-        <thead>
-          <tr>
-            <th>Claim Id</th>
-            <th>User Id</th>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Email</th>
-            <th>Phone Number</th>
-            <th>Date of Claim</th>
-            <th>Date of Damage</th>
-            <th>City</th>
-            <th>State</th>
-            <th class="image-link">Image Link</th>
-            <th>Description of Damage</th>
-            <th>Claim Status</th>
-          </tr>
-        </thead>
+        <table>
+          <thead>
+            <tr>
+              <th>Claim Id</th>
+              <th>User Id</th>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Email</th>
+              <th>Phone Number</th>
+              <th>Date of Claim</th>
+              <th>Date of Damage</th>
+              <th>City</th>
+              <th>State</th>
+              <th class="image-link">Image Link</th>
+              <th>Description of Damage</th>
+              <th>Claim Status</th>
+            </tr>
+          </thead>
 
-        <tbody>
-          <tr
-            v-for="claim in this.$store.state.claims"
-            v-bind:key="claim.claimId"
-          >
-            <td>{{ claim.claimId }}</td>
-            <td>{{ claim.userId }}</td>
-            <td>{{ claim.firstName }}</td>
-            <td>{{ claim.lastName }}</td>
-            <td>{{ claim.email }}</td>
-            <td>{{ claim.phoneNumber }}</td>
-            <td>{{ formatDate(claim.dateOfClaim) }}</td>
-            <td>{{ formatDate(claim.dateOfIncident) }}</td>
-            <td>{{ claim.locationOfIncidentCity }}</td>
-            <td>{{ claim.locationOfIncidentState }}</td>
-            <td><img v-bind:src="claim.imageLink" class="image-link" /></td>
-            <td>{{ claim.descriptionOfDamage }}</td>
-            <td>{{ claim.claimStatus }}</td>
-          </tr>
-        </tbody>
-      </table>
+          <tbody>
+            <tr
+              v-for="claim in this.$store.state.claims"
+              v-bind:key="claim.claimId"
+            >
+              <td>{{ claim.claimId }}</td>
+              <td>{{ claim.userId }}</td>
+              <td>{{ claim.firstName }}</td>
+              <td>{{ claim.lastName }}</td>
+              <td>{{ claim.email }}</td>
+              <td>{{ claim.phoneNumber }}</td>
+              <td>{{ formatDate(claim.dateOfClaim) }}</td>
+              <td>{{ formatDate(claim.dateOfIncident) }}</td>
+              <td>{{ claim.locationOfIncidentCity }}</td>
+              <td>{{ claim.locationOfIncidentState }}</td>
+              <td><img v-bind:src="claim.imageLink" class="image-link" /></td>
+              <td>{{ claim.descriptionOfDamage }}</td>
+              <td v-if="!isEditClicked">
+                {{ claim.claimStatus }}<br /><br />
+                <button v-if="!isEditClicked" v-on:click="changeIsEditClicked">
+                  Edit
+                </button>
+              </td>
+              <td v-if="isEditClicked">
+                <select
+                  id="claimStatus"
+                  name="claimStatus"
+                  v-model="claim.claimStatus"
+                >
+                  <option>Pending</option>
+                  <option>Approved</option>
+                  <option>Denied</option></select
+                ><br /><br />
+                <button v-on:click="updateClaim(claim)" v-if="isEditClicked">
+                  Submit Status
+                </button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     </div>
   </div>
@@ -53,9 +72,14 @@ import ClaimFormService from "../services/ClaimFormService.js";
 
 export default {
   data() {
-    return {};
+    return {
+      isEditClicked: false,
+    };
   },
   methods: {
+    changeIsEditClicked() {
+      this.isEditClicked = !this.isEditClicked;
+    },
     formatDate(date) {
       if (date != null) {
         const month = date.substring(5, 7);
@@ -66,6 +90,19 @@ export default {
       }
 
       return date;
+    },
+    updateClaim(claim) {
+      ClaimFormService.updateClaimStatus(claim)
+        .then((r) => {
+          if (r.status === 200) {
+            this.$store.commit("UPDATE_CLAIM", claim);
+            alert("Status Successfully Updated");
+            this.isEditClicked = !this.isEditClicked;
+          }
+        })
+        .catch((error) => {
+          console.log(error.response.status);
+        });
     },
   },
   created() {
@@ -88,6 +125,10 @@ export default {
 }
 
 .scroll-section {
-    overflow: auto;
+  overflow: auto;
+}
+
+button {
+  background-color: #adc178;
 }
 </style>
