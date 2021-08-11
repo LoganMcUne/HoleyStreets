e8f3b2e6aaf4f9b40ee38a9e9c14f1fe4c61df8b
John Sliney <jjs061090@gmail.com>
2021-08-11 14:41:51 -0400

    cleaned up addPothole

--- vue/src/components/AddPothole.vue
+++ vue/src/components/AddPothole.vue
@@ -1,36 +1,48 @@
 <template>
   <div class="form">
-      <b-button v-b-toggle.sidebar-1 class="add-new" v-if="$store.state.token != ''">Add A Pothole</b-button>
-      <b-sidebar id="sidebar-1" sidebar-class="border-right border-dark" text-variant="white" bg-variant="secondary" title="New Pothole" width="250px">
-        <div class="px-4 pb-2">
-          <form class="add-pothole" v-if="$store.state.token != ''">
-            <label for="latitude">Latitude:</label><br />
-            <input
-              type="text"
-              id="latitude"
-              name="latitude"
-              v-model.number="pothole.latitude"
-            /><br /><br />
-            <label for="longitude">Longitude:</label><br />
-            <input
-              type="text"
-              id="longitude"
-              name="longitude"
-              v-model.number="pothole.longitude"
-            /><br /><br />
-            <label for="image-link">Image Link:</label><br />
-            <input
-              type="url"
-              id="image-link"
-              name="image-link"
-              v-model="pothole.imageLink"
-            /><br /><br />
-            <button class="add-button" type="button" v-on:click="addNewPothole">
-              Add New Pothole
-            </button>
-          </form>
-        </div>
-      </b-sidebar>
+    <b-button
+      v-b-toggle.sidebar-1
+      class="add-new"
+      v-if="$store.state.token != ''"
+      >Add A Pothole</b-button
+    >
+    <b-sidebar
+      id="sidebar-1"
+      sidebar-class="border-right border-dark"
+      text-variant="white"
+      bg-variant="secondary"
+      title="New Pothole"
+      width="250px"
+    >
+      <div class="px-4 pb-2">
+        <form class="add-pothole" v-if="$store.state.token != ''">
+          <label for="latitude">Latitude:</label><br />
+          <input
+            type="text"
+            id="latitude"
+            name="latitude"
+            v-model.number="pothole.latitude"
+          /><br /><br />
+          <label for="longitude">Longitude:</label><br />
+          <input
+            type="text"
+            id="longitude"
+            name="longitude"
+            v-model.number="pothole.longitude"
+          /><br /><br />
+          <label for="image-link">Image Link:</label><br />
+          <input
+            type="url"
+            id="image-link"
+            name="image-link"
+            v-model="pothole.imageLink"
+          /><br /><br />
+          <button class="add-button" type="button" v-on:click="addNewPothole">
+            Add New Pothole
+          </button>
+        </form>
+      </div>
+    </b-sidebar>
   </div>
 </template>
 
@@ -43,27 +55,18 @@ export default {
   data() {
     return {
       pothole: {
-        latitude: "",
-        longitude: "",
+        latitude: this.currentCenter.lat,
+        longitude: this.currentCenter.lng,
         imageLink: "",
       },
-      variant: ""
+      variant: "",
     };
   },
   watch: {
-    currentCenter: function() {
-        this.pothole.latitude = this.currentCenter.lat;
-        this.pothole.longitude = this.currentCenter.lng;
-    }
-  },
-  computed: {
-    setLatAndLong() {
-      const splitCoords = this.currentCenter.split(',');
-      const currentLat = splitCoords[0].substring(1);
-      const currentLong = splitCoords[1].substring(0, splitCoords[1].length - 1);
-      this.setPotholeLatAndLong(currentLat, currentLong);
-      return '';
-    }
+    currentCenter: function () {
+      this.pothole.latitude = this.currentCenter.lat;
+      this.pothole.longitude = this.currentCenter.lng;
+    },
   },
   methods: {
     addNewPothole() {
@@ -73,29 +76,20 @@ export default {
         }
         this.resetPothole();
       });
-    },
+    },    
     resetPothole() {
-      this.pothole = {
-        latitude: this.pothole.latitude,
-        longitude: this.pothole.longitude,
-        imageLink: "",
-      };
+      this.pothole.imageLink = ""
     },
     setPotholes() {
       potholeService.list().then((r) => {
         this.$store.commit("SET_POTHOLE_LIST", r.data);
       });
     },
-    setPotholeLatAndLong(currentLat, currentLong) {
-      this.pothole.latitude = currentLat;
-      this.pothole.longitude = currentLong;
-    }
   },
 };
 </script>
 
 <style scoped>
-
 button.add-new {
   color: white;
   font-size: 20px;
